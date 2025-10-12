515deee5c56bd29ca358751e1ddba32114077a89
harshhala-ranii <harshalarani3@gmail.com>
2025-10-12 07:55:31 +0530

    Adding google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,6 +82,19 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" type="image/png" href="/icons/image.png" />
+        
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-25XSCBLL3D"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-25XSCBLL3D');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${comfortaa.variable} antialiased`}
